4af4d455dfbd102381ce99b96ce4a009d91da717
osegonte <segohopo@gmail.com>
2025-06-25 22:21:17 +0200

    55888600=

--- frontend/src/components/DebuggingPanel.jsx
+++ frontend/src/components/DebuggingPanel.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { resetStats } from '../services/api';
 
 const DebuggingPanel = ({ darkMode }) => {
   const [debugInfo, setDebugInfo] = useState(null);
